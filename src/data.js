24c19eaf69b7add25f07709993d99a8721149ebe
nikolaPokrajac <nikolapokrajac92@gmail.com>
2022-02-16 15:26:29 +0100

    replaced images

--- src/data.js
+++ src/data.js
@@ -1,62 +1,62 @@
 const photos = [
   {
-    url: './images/image-1.jpg',
+    url: 'https://images.unsplash.com/photo-1622901120958-ae569882629c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80',
     description: 'Image title 1',
   },
   {
-    url: './images/image-2.jpg',
+    url: 'https://images.unsplash.com/photo-1476900543704-4312b78632f8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=934&q=80',
     description: 'Image title 2',
   },
   {
-    url: './images/image-3.jpg',
+    url: 'https://images.unsplash.com/photo-1500530855697-b586d89ba3ee?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80',
     description: 'Image title 3',
   },
   {
-    url: './images/image-4.jpg',
+    url: 'https://images.unsplash.com/photo-1516483638261-f4dbaf036963?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=933&q=80',
     description: 'Image title 4',
   },
   {
-    url: './images/image-5.jpg',
+    url: 'https://images.unsplash.com/photo-1523906834658-6e24ef2386f9?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=930&q=80',
     description: 'Image title 5',
   },
   {
-    url: './images/image-6.jpg',
+    url: 'https://images.unsplash.com/photo-1533105079780-92b9be482077?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=934&q=80',
     description: 'Image title 6',
   },
   {
-    url: './images/image-7.jpg',
+    url: 'https://images.unsplash.com/photo-1534366428-e54c1db0bed4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=934&q=80',
     description: 'Image title 7',
   },
   {
-    url: './images/image-8.jpg',
+    url: 'https://images.unsplash.com/photo-1527631746610-bca00a040d60?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80',
     description: 'Image title 8',
   },
   {
-    url: './images/image-9.jpg',
+    url: 'https://images.unsplash.com/photo-1507608616759-54f48f0af0ee?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80',
     description: 'Image title 9',
   },
   {
-    url: './images/image-10.jpg',
+    url: 'https://images.unsplash.com/photo-1526315251174-de0107e28fb4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80',
     description: 'Image title 10',
   },
   {
-    url: './images/image-11.jpg',
+    url: 'https://images.unsplash.com/photo-1533105079780-92b9be482077?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=934&q=80',
     description: 'Image title 11',
   },
   {
-    url: './images/image-12.jpg',
+    url: 'https://images.unsplash.com/photo-1534366428-e54c1db0bed4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=934&q=80',
     description: 'Image title 12',
   },
   {
-    url: './images/image-13.jpg',
+    url: 'https://images.unsplash.com/photo-1527631746610-bca00a040d60?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80',
     description: 'Image title 13',
   },
   {
-    url: './images/image-14.jpg',
+    url: 'https://images.unsplash.com/photo-1507608616759-54f48f0af0ee?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80',
     description: 'Image title 14',
   },
   {
-    url: './images/image-15.jpg',
+    url: 'https://images.unsplash.com/photo-1526315251174-de0107e28fb4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80',
     description: 'Image title 15',
   },
 ];
